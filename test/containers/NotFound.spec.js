de658307c6a50ace8986eb726c21da2a0d432d67
Justin Robb <reptarsrage@gmail.com>
2019-05-13 15:42:49 -0700

    Cleaning up tests

--- test/containers/NotFound.spec.js
+++ test/containers/NotFound.spec.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import * as sinon from 'sinon';
 import Enzyme from 'enzyme';
 import { createMount } from '@material-ui/core/test-utils';
 import Adapter from 'enzyme-adapter-react-16';
 
-import About from '../../app/containers/About';
+import NotFound from '../../app/containers/NotFound';
 
-describe('About container', () => {
+describe('<NotFound />', () => {
   let mount;
 
   beforeAll(() => {
@@ -19,8 +18,8 @@ describe('About container', () => {
   });
 
   it('should should display header', () => {
-    const component = mount(<About history={{ goBack: sinon.stub() }} />);
+    const component = mount(<NotFound />);
     const header = component.find('h1');
-    expect(header.text()).toMatch(/^About$/);
+    expect(header.text()).toMatch(/^Not Found$/);
   });
 });
