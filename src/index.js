b54a745009983904e4b6fa460cf40ec5dcff8b6c
Justin Robb <reptarsrage@gmail.com>
2019-05-17 23:31:39 -0700

    Adding HMR and simplifying Root/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDom from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router/immutable';
 
-import Root from './containers/Root';
+import Routes from './Routes';
 import { configureStore, history } from './store/configureStore';
 import rootSaga from './sagas';
 import './index.css';
 
 const store = configureStore();
 store.runSaga(rootSaga);
+serviceWorker.register();
 
-render(<Root store={store} history={history} />, document.getElementById('root'));
+const render = Component =>
+  ReactDom.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Component />
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(Routes);
+
+if (module.hot) {
+  module.hot.accept('./Routes', () => {
+    const NextApp = require('./Routes').default;
+    render(NextApp);
+  });
+}
