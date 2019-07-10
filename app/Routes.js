0c802ee9097cc5402234cbe0cb0c0363ee8555d5
Justin Robb <reptarsrage@gmail.com>
2019-07-09 19:25:23 -0700

    Adding route transitions

--- app/Routes.js
+++ app/Routes.js
@@ -8,16 +8,17 @@ import Login from './containers/Login';
 import OAuth from './containers/OAuth';
 import About from './containers/About';
 import NotFound from './containers/NotFound';
+import WithTransition from './hocs/WithTransition';
 
 export default () => (
   <App>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login/:moduleId" component={Login} />
-      <Route path="/oauth/:moduleId" component={OAuth} />
-      <Route path="/gallery/:moduleId/:galleryId" component={Gallery} />
-      <Route path="/about" component={About} />
-      <Route path="*" component={NotFound} />
+      <Route exact path="/" component={WithTransition(Home)} />
+      <Route path="/login/:moduleId" component={WithTransition(Login)} />
+      <Route path="/oauth/:moduleId" component={WithTransition(OAuth)} />
+      <Route path="/gallery/:moduleId/:galleryId" component={WithTransition(Gallery)} />
+      <Route path="/about" component={WithTransition(About)} />
+      <Route path="*" component={WithTransition(NotFound)} />
     </Switch>
   </App>
 );
