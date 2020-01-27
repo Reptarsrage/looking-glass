2239bfd64039605b7333a300cdd5d4a7457a2f08
Justin Robb <jrobb@meredith.com>
2020-01-27 11:04:56 -0800

    Using better palette

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -20,9 +20,20 @@ import * as appActions from '../actions/appActions';
 import SearchBar from '../components/SearchBar';
 import BackButton from '../components/BackButton';
 
+// https://material-ui.com/customization/palette/
 const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: {
+      light: '#a6d4fa',
+      main: '#90caf9',
+      dark: '#648dae',
+    },
+    secondary: {
+      light: '#f6a5c0',
+      main: '#f48fb1',
+      dark: '#aa647b',
+    },
   },
   typography: {
     useNextVariants: true,
@@ -30,6 +41,19 @@ const darkTheme = createMuiTheme({
 });
 
 const lightTheme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#4791db',
+      main: '#1976d2',
+      dark: '#115293',
+    },
+    secondary: {
+      light: '#e33371',
+      main: '#dc004e',
+      dark: '#9a0036',
+    },
+  },
   typography: {
     useNextVariants: true,
   },
