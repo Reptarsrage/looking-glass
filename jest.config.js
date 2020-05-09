10d7199833d0c08633ca9e65c299bacecef5392d
Justin Robb <reptarsrage@gmail.com>
2020-05-08 20:52:45 -0700

    ci(jest.config.js): updating jest to report all coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test-setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json'],
   transform: {
     '.jsx?$': 'babel-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.*'],
 };
