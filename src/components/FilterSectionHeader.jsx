13190fce3201a46f6fbbcaa57d5e31fac8bfbe1a
Justin Robb <reptarsrage@gmail.com>
2021-09-26 15:43:31 -0700

    fix: fixing filter lsit subheader colors in light theme

--- src/components/FilterSectionHeader.jsx
+++ src/components/FilterSectionHeader.jsx
@@ -3,30 +3,34 @@ import PropTypes from 'prop-types'
 import ListSubheader from '@material-ui/core/ListSubheader'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import { useSelector } from 'react-redux'
+import { makeStyles } from '@material-ui/styles'
 
 import { filterSectionNameSelector, filterSectionFetchingSelector } from 'selectors/filterSectionSelectors'
 import { moduleFilterSectionsSelector } from 'selectors/moduleSelectors'
 
-export default function SectionHeader({ sectionIndex, moduleId }) {
+const useStyles = makeStyles((theme) => ({
+  subHeader: {
+    backgroundColor: theme.palette.background.paper,
+    backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))',
+  },
+}))
+
+export default function FilterSectionHeader({ sectionIndex, moduleId }) {
+  const classes = useStyles()
   const sections = useSelector((state) => moduleFilterSectionsSelector(state, { moduleId }))
   const filterSectionId = sections[sectionIndex]
   const header = useSelector((state) => filterSectionNameSelector(state, { filterSectionId }))
   const fetching = useSelector((state) => filterSectionFetchingSelector(state, { filterSectionId }))
 
   return (
-    <ListSubheader
-      style={{
-        backgroundColor: '#333333',
-        backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.15))',
-      }}
-    >
+    <ListSubheader className={classes.subHeader}>
       {header}
       {fetching && <LinearProgress color="primary" />}
     </ListSubheader>
   )
 }
 
-SectionHeader.propTypes = {
+FilterSectionHeader.propTypes = {
   sectionIndex: PropTypes.number.isRequired,
   moduleId: PropTypes.string.isRequired,
 }
