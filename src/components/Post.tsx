3aecf15d1e037eb1c54957aee4d3f74749c0b05f
Justin Robb <reptarsrage@gmail.com>
2022-08-09 22:52:17 -0700

    feat: hooking up lowDataMode settings

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,25 +1,63 @@
-import { useMemo, useState, memo, useContext } from "react";
+import { useRef, useMemo, useState, memo, useContext } from "react";
 import Paper from "@mui/material/Paper";
 
 import Video from "./Video";
 import Picture from "./Picture";
-import type { Post } from "../store/gallery";
+import type { Post, PostUrl } from "../store/gallery";
 import { SettingsContext } from "../store/settings";
+import useSize from "../hooks/useSize";
+import { useEffect } from "react";
 
 interface PostProps {
   post: Post;
   interactable?: boolean;
 }
 
+function findIndex(sortedUrls: PostUrl[], width: number, lowDataMode: boolean): PostUrl {
+  if (!lowDataMode) {
+    return sortedUrls[sortedUrls.length - 1];
+  }
+
+  let index = sortedUrls.findIndex((url) => url.width >= width);
+  if (index < 0) {
+    index = sortedUrls.length - 1;
+  }
+
+  return sortedUrls[index];
+}
+
 const PostElt: React.FC<PostProps> = ({ post, interactable }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const { settings } = useContext(SettingsContext);
+  const { width: containerWidth } = useSize(containerRef);
   const { isVideo, urls, poster, width, height, name } = post;
   const sortedUrls = useMemo(() => [...urls].sort((a, b) => a.width - b.width), [urls]);
-  const [curIndex] = useState(Math.max(0, sortedUrls.length - 2)); // TODO: choose best based on size
-  const source = sortedUrls[curIndex];
+  const [oldSource, setOldSource] = useState<PostUrl | null>(null);
+  const lowDataMode = isVideo ? settings.videoLowDataMode : settings.pictureLowDataMode;
+  const [source, setSource] = useState(findIndex(sortedUrls, containerWidth, lowDataMode));
+
+  // effect to set source based on width
+  useEffect(() => {
+    const newSource = findIndex(sortedUrls, containerWidth, lowDataMode);
+    if (newSource !== source) {
+      setOldSource(source);
+      setSource(newSource);
+    }
+  }, [containerWidth, lowDataMode]);
 
   return (
-    <Paper elevation={4} sx={{ borderRadius: 2, height: "100%", overflow: "hidden" }}>
+    <Paper
+      ref={containerRef}
+      elevation={4}
+      sx={{
+        borderRadius: 2,
+        height: "100%",
+        overflow: "hidden",
+        backgroundImage: !isVideo && oldSource ? `url(${oldSource.url})` : undefined,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       {isVideo ? (
         <Video
           loop
@@ -32,7 +70,7 @@ const PostElt: React.FC<PostProps> = ({ post, interactable }) => {
           source={source}
         />
       ) : (
-        <Picture loading="lazy" source={sortedUrls[curIndex]} alt={name} width={width} height={height} />
+        <Picture loading="lazy" source={source} alt={name} width={width} height={height} />
       )}
     </Paper>
   );
