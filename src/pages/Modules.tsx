18a1de4c75c3730b964a17bd0840a9dbb8442076
Justin Robb <reptarsrage@gmail.com>
2023-01-21 18:33:48 -0800

    fix: fixing modules lsit items styles

--- src/pages/Modules.tsx
+++ src/pages/Modules.tsx
@@ -54,15 +54,15 @@ function Modules() {
         <li key={module.id} className="border-b-2">
           <Link
             to={`/module/${module.id}`}
-            className="flex p-2 gap-2 items-center cursor-pointer transition duration-150 hover:bg-slate-50 active:bg-slate-200"
+            className="flex p-2 gap-2 items-center cursor-pointer transition duration-150 hover:bg-slate-50 active:bg-slate-200 group"
           >
-            <div className="rounded-full h-12 w-12 bg-slate-600 overflow-hidden flex shadow-md border">
+            <div className="rounded-full h-12 w-12 bg-slate-600 overflow-hidden flex shadow-md border transition-transform group-hover:scale-105 group-active:scale-95">
               <img src={module.icon} alt={module.id} />
             </div>
 
-            <div className="flex flex-col">
-              <span className="text-lg font-semibold">{module.name}</span>
-              <span>{module.description}</span>
+            <div className="flex flex-col flex-1">
+              <div className="text-lg font-semibold">{module.name}</div>
+              <div>{module.description}</div>
             </div>
           </Link>
         </li>
@@ -70,15 +70,15 @@ function Modules() {
       <li key={fileSystemModuleId} className="border-b-2">
         <button
           onClick={handleLocalClick}
-          className="flex p-2 w-full gap-2 items-center cursor-pointer transition duration-150 hover:bg-slate-50 active:bg-slate-200"
+          className="flex p-2 w-full gap-2 items-center cursor-pointer text-left transition duration-150 group hover:bg-slate-50 active:bg-slate-200"
         >
-          <div className="rounded-full h-12 w-12 bg-blue-500 overflow-hidden flex text-white p-2 shadow-md border">
+          <div className="rounded-full h-12 w-12 bg-blue-500 overflow-hidden flex text-white p-2 shadow-md border transition-transform group-hover:scale-105 group-active:scale-95">
             <FolderIcon />
           </div>
 
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold">{fileSystemModule.name}</span>
-            <span>{fileSystemModule.description}</span>
+          <div className="flex flex-col flex-1">
+            <div className="text-lg font-semibold">{fileSystemModule.name}</div>
+            <div>{fileSystemModule.description}</div>
           </div>
         </button>
       </li>
