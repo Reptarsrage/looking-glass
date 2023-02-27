5dbc4b7dca8efb8f6db1ebd55d63e5b4ac9331fc
Justin Robb <reptarsrage@gmail.com>
2023-02-26 17:19:18 -0800

    build: fixing some issues with git formattting and linting

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 & npm install
+& npm run clean
 & npm run build
-& npm run dist -w
+& npm run dist
 
-$installer = Get-ChildItem -Path .\release\ -Filter "The Looking-Glass Setup *.exe" | Select-Object -First 1
+$installer = Get-ChildItem -Path .\dist\ -Filter "The Looking-Glass Setup *.exe" | Select-Object -First 1
 & $installer.FullName
